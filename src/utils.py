14174565199d9b899a0389f3fd9756a89809bbe7
ekolima <t8160047@aueb.gr>
2023-12-07 21:56:21 +0200

    moved extract_ratings to utils

--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,7 @@ import numpy as np
 def np_encoder(object):
     if isinstance(object, np.generic):
         return object.item()
+
+
+def extract_ratings(id, ratings, id_type='user id'):
+    return ratings[ratings[id_type] == id]
