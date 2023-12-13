7b32fbbacb12469971139e1109bf064d594a5bc0
ekolima <t8160047@aueb.gr>
2023-12-13 22:55:19 +0200

    item item fix and added comments

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 from metrics import *
 from utils import extract_ratings
 
@@ -33,13 +32,16 @@ def user_user(id, data, n, similarity=jaccard, return_scores=False):
     if user_ratings.empty:
         return None
 
+    # get unique users
     users = pd.DataFrame(data[['user id']].drop_duplicates())
+    # calculate similarity
     if similarity.__name__ in ['jaccard', 'dice']:
         users['similarity'] = users['user id'].apply(lambda x: similarity(user_ratings, extract_ratings(x, data),
                                                                           extract_column='item id'))
     elif similarity.__name__ in ['cosine', 'pearson']:
         users['similarity'] = users['user id'].apply(lambda x: similarity(user_ratings, extract_ratings(x, data),
                                                                           extract_column='rating', id_col='item id'))
+    # calculate weighted similarity score from the k most similar users
     result = n_most_similar(users.loc[users['user id'] != id], data, user_ratings)
 
     if return_scores:
@@ -48,28 +50,39 @@ def user_user(id, data, n, similarity=jaccard, return_scores=False):
         return result.sort_values('weighted_rating', ascending=False).head(n)['item id'].tolist()
 
 
-# ratings
-def item_item(id, data, n, similarity=pearson_matrix, return_scores=False):
+def item_item(id, data, n, similarity=pearson, return_scores=False):
+    # extract ratings for user
     user_ratings = extract_ratings(id, data)
     if user_ratings.empty:
         return None
 
-    user_rated_items = user_ratings['item id'].tolist()
-    user_not_rated_items = pd.DataFrame(data.loc[~data['item id'].isin(user_rated_items)]['item id'].drop_duplicates())
-    if similarity.__name__ in ['jaccard', 'dice']:
-        user_not_rated_items['similarity'] = user_not_rated_items['item id'].apply(lambda x: similarity(user_ratings, extract_ratings(x, data),
-                                                                          extract_column='item id'))
-    elif similarity.__name__ in ['cosine', 'pearson_matrix']:
-        user_rated_items = user_ratings['item id'].tolist()
-        user_not_rated_items = data.loc[~data['item id'].isin(user_rated_items)]['item id'].unique()
-        unknown_ratings = []
-        for item in user_not_rated_items:
-            matrix = data[data['item id'].isin([item]+user_rated_items)]
-            matrix = matrix.pivot(columns='user id', index='item id', values='rating').fillna(0)
-            similarities = similarity(matrix, item)
-            final = pd.DataFrame(similarities, columns=['similarity']).merge(user_ratings.loc[:,['item id', 'rating']], on='item id')
-            unknown_ratings.append(sum(final['similarity'] * final['rating'])/sum(final['similarity']))
-        final_ratings = pd.DataFrame({'item id': user_not_rated_items, 'weighted_rating': unknown_ratings})
+    # get matrix similarity function
+    similarity = eval(similarity.__name__ + '_similarity_matrix')
+    # pivot data
+    data_matrix = pd.pivot(data, columns='user id', index='item id', values='rating')
+
+    # calculate similarity
+    similarity_matrix = similarity(data_matrix)
+    similarity_matrix = similarity_matrix.unstack()
+
+    # keep top k similar items
+    top_similar_users = similarity_matrix.groupby(level=0).apply(lambda x: x.nlargest(128))
+    top_similar_users.index = top_similar_users.index.droplevel(1)
+
+    top_similar_users.index.names = ['item id 1', 'item id 2']
+    top_similar_users = top_similar_users.reset_index()
+    top_similar_users.columns = ['item id 1', 'item id', 'similarity']
+
+    # merge ratings with similarity scores
+    top_similar_users = top_similar_users.merge(user_ratings[['item id', 'rating']], on='item id', how='outer')
+    top_similar_users.fillna(0, inplace=True)
+    # calculate weighted similarity score
+    top_similar_users['weighted_rating'] = top_similar_users['similarity'] * top_similar_users['rating']
+    weighted_avg = top_similar_users.groupby('item id 1', as_index=False).apply(lambda x: x['weighted_rating'].sum()/x['similarity'].sum())
+    weighted_avg.columns = ['item id', 'weighted_rating']
+
+    # remove items already rated by user
+    final_ratings = weighted_avg.loc[~weighted_avg.index.isin(user_ratings['item id'])]
 
     if return_scores:
         return final_ratings.sort_values('weighted_rating', ascending=False).head(n).set_index('item id')['weighted_rating'].to_dict()
@@ -96,7 +109,7 @@ def tag_based(id, data, n, similarity=jaccard, return_scores=False):
 
     tag_similarities.columns = ['movie id', 'similarity']
 
-    # find n most similar movies
+    # return n most similar movies
     if return_scores:
         return tag_similarities.loc[tag_similarities['movie id'] != id].sort_values('similarity', ascending=False).head(n).set_index('movie id')['similarity'].to_dict()
     else:
@@ -127,7 +140,6 @@ def calculate_tf_idf(data):
 
 # keywords
 def content_based(id, data, n, similarity=jaccard, return_scores=False):
-
     # get tf-idf for each movie/keyword
     keywords_frequency_per_movie = calculate_tf_idf(data)
 
@@ -153,14 +165,13 @@ def content_based(id, data, n, similarity=jaccard, return_scores=False):
         return keyword_similarities.loc[keyword_similarities['movie id'] != id].sort_values('similarity', ascending=False).head(n)['movie id'].to_list()
 
 
-# combine all methods
 def hybrid(id, data, n, similarity=jaccard, return_scores=False):
-    # data
+    # get data
     ratings, tags = data['ratings'], data['tags']
 
     # get top recommendation score using user-user and item-item
     user_user_recommendations = user_user(id, ratings, None, similarity, return_scores=True)
-    item_item_recommendations = item_item(id, ratings, None, pearson_matrix, return_scores=True)
+    item_item_recommendations = item_item(id, ratings, None, similarity, return_scores=True)
 
     # merge user user and item item scores
     user_user_recommendations = pd.DataFrame({'movie id': list(user_user_recommendations.keys()),
@@ -179,14 +190,18 @@ def hybrid(id, data, n, similarity=jaccard, return_scores=False):
     # get tag based recommendations for top N*5 recommendations
     tag_based_results = dict()
     for m in all_recommendations['movie id'].to_list():
-        print(m)
+        # print(m)
         tag_based_results[m] = tag_based(m, tags, None, similarity, return_scores=True)
 
+    # pivot data longer
     tag_based_results_long = pd.DataFrame(tag_based_results).unstack().reset_index()
     tag_based_results_long.columns = ['movie id', 'movie suggested', 'score']
+    # remove movies that have been already rated
     tag_based_results_long = tag_based_results_long.loc[tag_based_results_long['movie id'] != tag_based_results_long['movie suggested']]
+    # remove movies with zero score
     tag_based_results_long = tag_based_results_long.loc[tag_based_results_long['score'] > 0]
 
+    # sum tag based scores per movie
     tags_score = tag_based_results_long.groupby(['movie suggested'], as_index=False).agg({'score': 'sum'})
     tags_score.columns = ['movie id', 'score']
 
